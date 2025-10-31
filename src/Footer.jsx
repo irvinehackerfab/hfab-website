258b886f4b359c802dc684b4be34bf39e6b1eb0b
Kevin Li <kevinli200555@gmail.com>
2025-10-30 23:02:19 -0700

    Different style changes, color variables, and css animations

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -1,12 +1,11 @@
 import { FaInstagram, FaDiscord, FaGithub} from "react-icons/fa";
-import { MdEmail } from "react-icons/md";
+import { CiCircleMore } from "react-icons/ci";
 import "./Footer.css"
 function Footer() {
     const discordLink = "https://discord.gg/p2byDUhDAz";
     const instagramLink = "https://www.instagram.com/uci.hfab/";
     const gitHubLink = "https://github.com/hacker-fab/gitbook";
-    const mailLink = "kevinli200555@gmail.com"
-    const gmailUrl = `https://mail.google.com/mail/?view=cm&fs=1&to=${mailLink}`;
+    const linkTreeLink = "https://linktr.ee/IrvineHackerFab?fbclid=PAZXh0bgNhZW0CMTEAAac_6Bug9u7huxEPeX3qgwWg3h8uP4Y9Y2oZP4DeIv7vDJJ16qKBxogOCIhpxQ_aem_qt6oMqUrW0fsw9oy6V3oDA";
     return (
         <>
             <div className="Footer">
@@ -19,6 +18,9 @@ function Footer() {
                     </a>
                     <a href={`${gitHubLink}`} target="_blank" rel="noopener noreferrer">
                         <FaGithub className="socialButton" />
+                    </a>
+                    <a href={`${linkTreeLink}`} target="_blank" rel="noopener noreferrer">
+                        <CiCircleMore className="socialButton" />
                     </a>
                 </div>
                 <h1 className = "bodyText">UCI Facker Hab 2025</h1>
--- src/NavBar.jsx
+++ src/NavBar.jsx
@@ -7,19 +7,30 @@ function NavBar() {
   // state variable to keep track of what page we're on
   // the li changes design based on if it's on the page
   const location = useLocation();
+  // background color changes based on if we're on home page or not
+  // the home paege has a white background, other pages have blue
+  const headerStyle = {
+    backgroundColor: location.pathname !== "/" ? "var(--primary-blue)" : "white"};
+  const listStyle = (navBarItem) =>{
+    return location.pathname === "/" ? "navbarListItem" : location.pathname === `/${navBarItem}` ? "navbarListItem": "navbarListItemSelected"
+  };
   return (
-    <header>
+    <header style={headerStyle}>
       {/* Logo Image */}
-      <img id="navbarLogo" src={logo} alt="Irvine Hacker Fab"></img>
+      <div id = "logoContainer">
+        <img id="navbarLogo" src={logo} alt="Irvine Hacker Fab"></img>
+        <h1>Cool club tagline</h1>
+      </div>
       <nav>
         {/* list of navigation links */}
         <ul id="navBarList">
-          {/* If the current path matches the link path, apply the 'selected' style (change to blue)*/}
-          <li className={location.pathname === "/" ? "navbarListItemSelected" : "navbarListItem"}><Link to={"/"}>Home</Link></li>
-          <li className={location.pathname === "/About" ? "navbarListItemSelected" : "navbarListItem"}><Link to={"/About"}>About</Link></li>
-          <li className={location.pathname === "/Subteams" ? "navbarListItemSelected" : "navbarListItem"}><Link  to = {"/Subteams"}>Subteams</Link></li>
-          <li className={location.pathname === "/HowtoContribute" ? "navbarListItemSelected" : "navbarListItem"}><Link to = {"/HowtoContribute"}>How To Contribute</Link></li>
-          <li className={location.pathname === "/Resources" ? "navbarListItemSelected" : "navbarListItem"}><Link to = {"/Resources"}>Resources</Link></li>
+          {/*If the list item is selected, it will be white, if not selected, it will be blue*/}
+          {/*The exception is the home page, on the home page, the home page navitem will be blue, the others will be white*/}
+          <li className={"navbarListItemSelected"}><Link to={"/"}>Home</Link></li>
+          <li className={listStyle("About")}><Link to={"/About"}>About</Link></li>
+          <li className={listStyle("Subteams")}><Link  to = {"/Subteams"}>Subteams</Link></li>
+          <li className={listStyle("HowtoContribute")}><Link to = {"/HowtoContribute"}>How To Contribute</Link></li>
+          <li className={listStyle("Resources")}><Link to = {"/Resources"}>Resources</Link></li>
         </ul>
       </nav>
     </header>
