df9a496640b3d1db285363540967a0ad3399d771
Kevin Li <kevinli200555@gmail.com>
2025-10-31 11:24:42 -0700

    Added a board members section we can add more board memebers in the array later

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -3,26 +3,54 @@ import "./About.module.css";
 import team_photo from "../assets/old_team_photo.jpg";
 import Footer from "../Footer";
 import styles from "./About.module.css";
+import logo from "../assets/hf_logo.png";
 function About() {
+  const BoardMembers = [
+    { name: 'Peter', role: 'President', year: '3rd Year Computer Engineering', image: logo },
+    { name: 'Peter', role: 'President', year: '3rd Year Computer Engineering', image: logo },
+    { name: 'Peter', role: 'President', year: '3rd Year Computer Engineering', image: logo },
+    { name: 'Peter', role: 'President', year: '3rd Year Computer Engineering', image: logo },
+    { name: 'Peter', role: 'President', year: '3rd Year Computer Engineering', image: logo },
+    { name: 'Peter', role: 'President', year: '3rd Year Computer Engineering', image: logo },
+    { name: 'Peter', role: 'President', year: '3rd Year Computer Engineering', image: logo },
+    { name: 'Peter', role: 'President', year: '3rd Year Computer Engineering', image: logo },
+
+  ];
   return (
     <>
-    <div className="PageContainer">
-      <div className = {`${styles.ContentContainter}`}>
-        <img src={team_photo} alt="Team Photo"/>
+      <div className="PageContainer">
+        <div className={`${styles.ContentContainter}`}>
+          <img src={team_photo} alt="Team Photo" />
+        </div>
+        <div className="TextContainer">
+          <h1>ABOUT US</h1>
+          <p className="BoldBodyText">Who are we?</p>
+          <p className="BodyText">
+            The Irvine Hacker Fab is an interdisciplinary team dedicated to creating open-source solutions for microchip fabrication. As part of a broader network that includes CMU, OSU, UW, and MIT, we aim to democratize semiconductor manufacturing by developing innovative, cost-effective tools and materials.
+          </p>
+          <p className="BoldBodyText">What do we do?</p>
+          <p className="BodyText">
+            Like TSMC and Intel, we develop both the hardware and software behind chipmaking — but with a student-driven, DIY twist. From designing spin coaters and lithography systems to writing control software, our subteams work on real-world engineering challenges across disciplines. There’s something here for every major.
+          </p>
+        </div>
       </div>
-      <div className="TextContainer">
-        <h1>ABOUT US</h1>
-        <p className = "BoldBodyText">Who are we?</p>
-        <p className="BodyText">
-          The Irvine Hacker Fab is an interdisciplinary team dedicated to creating open-source solutions for microchip fabrication. As part of a broader network that includes CMU, OSU, UW, and MIT, we aim to democratize semiconductor manufacturing by developing innovative, cost-effective tools and materials.
-        </p>
-        <p className = "BoldBodyText">What do we do?</p>
-        <p className="BodyText">
-          Like TSMC and Intel, we develop both the hardware and software behind chipmaking — but with a student-driven, DIY twist. From designing spin coaters and lithography systems to writing control software, our subteams work on real-world engineering challenges across disciplines. There’s something here for every major.
-        </p>
+      <div className={`${styles.BoardMembersContainer}`}>
+        <div className="TextContainer">
+          <h1>BOARD MEMBERS</h1>
+        </div>
+        <div className={`${styles.BoardMembersIconContainer}`}>
+
+          {BoardMembers.map((member, index) => (
+            <div key={index} className={styles.boardCard}>
+              <img src={member.image} alt={member.name} className={styles.boardImage} />
+              <h3 className = {`${styles.member}`}>{member.name}</h3>
+              <p className = {`${styles.year}`}>{member.year}</p>
+              <p className = {`${styles.role}`}>{member.role}</p>
+            </div>
+          ))}
+        </div>
       </div>
-    </div>
-    <Footer></Footer>
+      <Footer></Footer>
     </>
   );
 }
