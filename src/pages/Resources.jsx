6f0c620d425bc9a6eaf62f38274f4babbc39464c
iKAN2025 <142475176+iKAN2025@users.noreply.github.com>
2025-11-17 17:54:26 -0800

    add starter code for resources

--- src/pages/Resources.jsx
+++ src/pages/Resources.jsx
@@ -1,17 +1,41 @@
 import React from "react";
+import styles from "./Resources.module.css";
 import Footer from "../Footer";
+
 function Resources() {
+  const gitbookLink = "https://docs.hackerfab.org/home/";
+
   return (
     <>
-      <div className="PageContainer">
-        <div className="TextContainer">
+      {/* Page Container */}
+      <div className={`${styles.ResourcesPageContainer}`}>
+        {/* Header */}
+        <div className={`${styles.ResourcesHeaderContainer}`}>
           <h1>Resources</h1>
         </div>
 
-      </div>
-      <Footer></Footer>
-    </>
+        {/* Cards Section */}
+        <div className={`${styles.ResourcesCardsContainer}`}>
+          {/* Empty card for spacing */}
+          <div className={`${styles.ResourceCard}`}></div>
 
+          {/* Gitbook Card */}
+          <div className={`${styles.ResourceCard}`}>
+            <p className={`${styles.ResourceLink}`}>
+              <a href={gitbookLink} target="_blank" rel="noopener noreferrer">
+                {gitbookLink}
+              </a>
+            </p>
+            <h2 className={`${styles.ResourceLabel}`}>Gitbook</h2>
+          </div>
+
+          {/* Empty card for spacing */}
+          <div className={`${styles.ResourceCard}`}></div>
+        </div>
+      </div>
+
+      <Footer />
+    </>
   );
 }
 
