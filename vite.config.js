b27e3b7088fbcbc4901638b9ec9db90b92de0641
iKAN2025 <bellsparents@gmail.com>
2026-06-02 11:20:01 -0700

    add

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/', // fix for deployment on eng.uci.edy
+  base: '/', // fix for deployment on eng.uci.edu and stuff
 })
